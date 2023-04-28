c34d99388efd8d1192ec0e652da5178dbf8ac0c7
PedroTFX <pedro.trindade.tfx@gmail.com>
2023-04-28 15:27:04 +0100

    item list working (no BD) items in item list are now always the same size fixed bug where item-detais would appear when typing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,7 @@ import { ProfileComponent } from './profile/profile.component';
 		FooterComponent,
 		CreateAccountComponent,
 		LoginComponent,
-  				DashboardComponent,
+  		DashboardComponent,
 		ItemSearchComponent,
   		ItemDetailComponent,
   		ProfileComponent
