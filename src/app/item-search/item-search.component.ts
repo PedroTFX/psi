289aae7c5b781e3ad3ba1277ec13e70683b83f23
fc56379 <103432891+dan110102@users.noreply.github.com>
2023-04-28 19:00:43 +0100

    Created item-search component

---
+++ src/app/item-search/item-search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-search',
+  templateUrl: './item-search.component.html',
+  styleUrls: ['./item-search.component.css']
+})
+export class ItemSearchComponent {
+
+}
