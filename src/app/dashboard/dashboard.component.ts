f919f89b5588cf66436deb887051b5e00ac51d72
fc56326 <marcusargomes@gmail.com>
2023-04-28 16:59:21 +0100

    Added dashboard service and route

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Profile } from '../types/Profile';
+import { DashboardService } from '../dashboard.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+	dashboard: Profile | null = null;
 
+	constructor(private dashboardService: DashboardService, private router: Router) { }
+
+	ngOnInit() {
+		this.dashboardService.get().subscribe((result) => {
+			const { error, ...dashboard } = result
+			if (error) {
+				this.router.navigate(['/login'])
+			}
+			this.dashboard = dashboard
+			console.log(dashboard)
+
+		})
+	}
 }
