c2860c243a381c7e835971b613d222996f4b3008
fc56379 <103432891+dan110102@users.noreply.github.com>
2023-04-29 21:55:29 +0100

    Fixes in backend

--- backend/models/GameListModel.js
+++ backend/models/GameListModel.js
@@ -1,18 +1,9 @@
-/**
- * @fileoverview Model for the GameList collection in the database.
- */
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema
 
-const gameListSchema = new Schema({
-  // _id: String,
-  creator: { type: Schema.Types.ObjectId, ref: 'User' },
-  followers: [{ type: Schema.Types.ObjectId, ref: 'User' }], 
-  name: String,
-  description: String,
-  games: [{ type: Schema.Types.ObjectId, ref: 'Game' }]
-})
+const GameList = mongoose.model('GameList', new mongoose.Schema({
+	userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+	name: { type: String, required: true },
+	games: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Game' }]
+}));
 
-const GameList = mongoose.model('GameList', gameListSchema);
-
-module.exports = GameList
+module.exports = { GameList }
--- backend/models/ProfileModel.js
+++ backend/models/ProfileModel.js
@@ -1,15 +1,13 @@
-// Its the Mongoose model that represents a document in the heroes collection of the MongoDB database
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema
 
-const profileSchema = new Schema({
-    // _id: String, // _id: is the one that is on the userSchema (both _id are the same)
-    // name: is the one that is on the userSchema (both name are the same)
-    image: Buffer,
-    lists: [{ type: Schema.Types.ObjectId, ref: 'GameList' }],
-    library: [{ type: Schema.Types.ObjectId, ref: 'Game' }]
-})
+const Profile = mongoose.model('Profile', new mongoose.Schema({
+	userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+	username: { type: String, required: true, unique: true},
+	image: String,
+	library: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Game' }],
+	lists: [{ type: mongoose.Schema.Types.ObjectId, ref: 'GameList' }],
+	following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Profile' }],
+	followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Profile' }]
+}))
 
-const Profile = mongoose.model('Profile', profileSchema)
-
-module.exports = Profile
+module.exports = { Profile }
