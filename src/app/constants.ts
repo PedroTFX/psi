c2ddfd28215fe4cf861cec5049c258a15b48105f
56380 <joao.bf4@outlook.com>
2023-05-13 17:02:38 +0100

    api string appserver

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,2 +1,2 @@
-//export const api = 'http://appserver.alunos.di.fc.ul.pt:3055/api'
-export const api = 'http://localhost:3055/api'
+export const api = 'http://appserver.alunos.di.fc.ul.pt:3055/api'
+//export const api = 'http://localhost:3055/api'
